cf4972481393eb0a4e082ac1d0cf42f47b526bfc
seiyo1 <seiyo.03oo@gmail.com>
2025-10-28 21:13:55 +0900

    ✨ feat(auth): passportによるregister/login/logout機能の実装

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
 
 Route::prefix('v1')->group(function () {
 Route::get('/todo', function () {});
