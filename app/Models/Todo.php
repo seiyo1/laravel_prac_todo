734e4fea2618be60980bd45a26c9d5e85fe9aadf
seiyo1 <seiyo.03oo@gmail.com>
2025-10-30 23:25:08 +0900

    ✨ feat(todo): todo APIのCRUD機能を実装

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,14 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Todo extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'title',
         'description',
         'completed',
     ];
+
+    /**
+     * 所有者ユーザーのリレーション。
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
